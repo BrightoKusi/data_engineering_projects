d2203741c978d75e026ab83ad4272c2b4a39c7c3
Bright Kusi <brightokusi@gmail.com>
2025-06-09 17:19:37 +0000

    Smart-waste project

---
+++ xmlr.py
@@ -0,0 +1,17 @@
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Featured Post -->
+
+						<article class="post featured">
+							<header class="major">
+								<h2><a href="https://github.com/BrightoKusi/azure-databricks-formula1-project.git">Azure Databricks Formula1 Project
+								</a></h2>
+								<p>This project utilizes Databricks, a cloud-based platform for data processing, to analyze Formula 1 racing data<br /> Dependencies: Databricks, Pyspark</p>
+							</header>
+							<a href="https://github.com/BrightoKusi/azure-databricks-formula1-project.git" class="image main"><img src="images/databricksup.png" alt="" /></a>
+							<ul class="actions special">
+								<li><a href="https://github.com/BrightoKusi/azure-databricks-formula1-project.git" class="button large">Full Project</a></li>
+							</ul>
+						</article>
